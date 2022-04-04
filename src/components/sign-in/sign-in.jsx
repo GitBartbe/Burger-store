fa874a8cd0c76b0adfbc9a7aca0d220372ac5297
GitBartbe <Bartosz.bednarski@wp.pl>
2022-04-04 23:14:05 +0200

    added styling to FormInput

---
+++ src/components/sign-in/sign-in.jsx
@@ -0,0 +1,13 @@
+import FormInput from '../form-input/form-input'
+import './sign-in.styles.scss'
+
+const SignIn = () => {
+  return (
+    <div>
+    <FormInput/>
+      
+    </div>
+  )
+}
+
+export default SignIn
